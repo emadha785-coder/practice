afa22d7b5207e1d1d7fda701e51b6de86757a8a5
root <emadha785@gmail.com>
2026-04-10 00:15:07 +0300

    Name of the customr added

--- random_ID.py
+++ random_ID.py
@@ -1,4 +1,6 @@
 import random
 
+name = input("Enter your name: ")
+print(f"Hello {name}!")
 print(f"\x1b[31;1m\t\t\tTrack Your order from Home: \x1b[0m")
 print(f"Your ID is:\x1b[32;1m{random.randint(1000000000,9999999999)}\x1b[0m")
